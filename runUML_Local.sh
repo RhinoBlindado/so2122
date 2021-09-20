0609a364b81eb3bc88b8e261a2b5a958e08b245e
Valentino Lugli <valentinolugli@gmail.com>
2021-09-20 22:48:32 +0200

    [S1][docs] Adding docs & scripts

---
+++ runUML_Local.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+# [CASTELLANO]
+# 
+# Descripción: Pequeño script que inicia una maquina virtual de Fedora con UML
+# Asignatura: Sistemas Operativos
+# Autor: Valentino Lugli
+# Fecha: Septiembre 2021
+# 
+# [ENGLISH]
+# 
+# Description: Small script that starts up a Fedora VM using UML
+# Course: Sistemas Operativos
+# Author: Valentino Lugli
+# Date: September 2021
+
+
+fedoraKernel/kernel32-3.0.4 ubda=fedoraKernel/Fedora14-x86-root_fs mem=1024m
